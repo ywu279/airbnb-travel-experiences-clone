2efa8a182c0567d1b6a2d8f5bf672dab9120ff24
Yufan Wu <wu000284@algonquinlive.com>
2022-07-03 22:35:45 -0400

    Updates

---
+++ precache-manifest.6a3c9d63dbe86ebab71fea173cc5c284.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9ffdd039f8d5bc65653",
+    "url": "/airbnb-travel-experiences-clone/static/css/main.00ece00c.chunk.css"
+  },
+  {
+    "revision": "b9ffdd039f8d5bc65653",
+    "url": "/airbnb-travel-experiences-clone/static/js/main.b9ffdd03.chunk.js"
+  },
+  {
+    "revision": "5090cfd648c5fe855a0c",
+    "url": "/airbnb-travel-experiences-clone/static/js/1.5090cfd6.chunk.js"
+  },
+  {
+    "revision": "a41638c37a5433393456",
+    "url": "/airbnb-travel-experiences-clone/static/js/runtime~main.a41638c3.js"
+  },
+  {
+    "revision": "ed422341e28f35fe26241b8004e2911a",
+    "url": "/airbnb-travel-experiences-clone/static/media/photo-grid.ed422341.png"
+  },
+  {
+    "revision": "7605d47d7aa508a7b6c059196411072c",
+    "url": "/airbnb-travel-experiences-clone/static/media/star.7605d47d.svg"
+  },
+  {
+    "revision": "df8eeefc964f5f812e903f995dac866d",
+    "url": "/airbnb-travel-experiences-clone/static/media/LocationIcon.df8eeefc.svg"
+  },
+  {
+    "revision": "8ed1231058f0260906f66f24ead2b9b2",
+    "url": "/airbnb-travel-experiences-clone/index.html"
+  }
+];
